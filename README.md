14328ee8ed1f5beb0bdb7acf50447f15146f84ee
Léana 江 <leana.jiang+git@icloud.com>
2025-11-15 12:16:39 +0800

    Branch off to make devshell more flexible

--- README.md
+++ README.md
@@ -4,11 +4,8 @@ This is a simple Nix developer environment for [hacking on Cabal](https://github
 
 The most convenient way to use it is to have [`direnv` hooked into your shell](https://direnv.net/docs/hook.html) and a `.envrc` in the root of your `cabal` checkout that looks like:
 
+The pins are added with the help of <https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=ghc>.
+
 ```bash
-# https://github.com/nix-community/nix-direnv A fast, persistent use_nix/use_flake implementation for direnv:
-if ! has nix_direnv_version || ! nix_direnv_version 2.3.0; then
-  source_url "https://raw.githubusercontent.com/nix-community/nix-direnv/2.3.0/direnvrc" "sha256-Dmd+j63L84wuzgyjITIfSxSD57Tx7v51DMxVZOsiUD8="
-fi
-# https://github.com/yvan-sraka/cabal.nix Slightly opinionated Cabal developer environment
-use flake "github:yvan-sraka/cabal.nix"
+nix-shell <path to this repo> --argstr ghcVersion "ghcxxx" # use a custom ghc version you want
 ```
--- shell.nix
+++ shell.nix
@@ -1,17 +1,48 @@
-{ pkgs ? import <nixpkgs> { } }:
+let
+  sources = import ./npins;
+
+  ghcVersionDefault = "ghc966";
+in
+{
+  pkgs ?
+    if builtins.hasAttr ghcVersion sources then
+      import sources.${ghcVersion} { }
+    else
+      throw "${ghcVersion} is not pinned, try pinning it or passing a nixpkgs instance instead",
+
+  ghcVersion ? ghcVersionDefault, # the version chosen by upstream cabal.nix
+
+  withHLS ? false,
+}:
+
+let
+  # The toolings default to ghc966
+  haskellPackagesDefault =
+    (import sources.${ghcVersionDefault} { }).haskell.packages.${ghcVersionDefault};
+
+  # GHC version to work with cabal is overridable
+  haskellPackages = pkgs.haskell.packages.${ghcVersion};
+
+  inherit (pkgs.haskell.lib)
+    justStaticExecutables
+    dontCheck
+    ;
+in
+
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [
-    (haskell.lib.justStaticExecutables (haskellPackages.cabal-install))
-    ghc
-    ghcid
-    haskell-language-server
-    (haskell.lib.dontCheck (haskellPackages.callHackage "fourmolu" "0.12.0.0" {}))
+  nativeBuildInputs = with pkgs; [
+    # GHC dependent
+    (lib.optional withHLS haskellPackages.haskell-language-server)
+    haskellPackages.ghc
+
+    (justStaticExecutables (dontCheck (haskellPackagesDefault.callHackage "fourmolu" "0.12.0.0" { })))
+    haskellPackagesDefault.cabal-install
+    haskellPackagesDefault.fix-whitespace
+    haskellPackagesDefault.hlint
+    haskellPackagesDefault.apply-refact
+    haskellPackagesDefault.doctest
+    haskellPackagesDefault.ghcid
     pkg-config
     zlib.dev
-
-    haskellPackages.fix-whitespace
-    haskellPackages.hlint
-    haskellPackages.apply-refact
-    haskellPackages.doctest
   ];
 }

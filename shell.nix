c84ae7da2bf1b1f08b2266ff46b183f8ebffc202
Léana 江 <leana.jiang+git@icloud.com>
2026-06-02 14:21:42 +0200

    pin fourmolu and hlint

--- shell.nix
+++ shell.nix
@@ -12,13 +12,43 @@ in
 
 let
   ghcs-nix = import sources.ghcs-nix;
+  pkgs_stackage_22_43 = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "b6b38bb0649cb8393492949e01b1e331c3a05718";
+    hash = "sha256-D408QVHL43RPYdPnGhTpBHJs4wVIebbsmh/SAAY0vAE=";
+  }) { };
+
+  pkgs_stackage_24_38 = import (pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "7fdb15681cb5daa386e25abe7ce611d2744ecc83";
+    hash = "sha256-QGbXhs6gSMg8w+kNco0UY5RVX0V1GWy+B/HGGPfU3dY=";
+  }) { };
+in
+
+let
   inherit (pkgs) lib;
-  inherit (pkgs) haskellPackages;
-  inherit (pkgs.haskell.lib) justStaticExecutables;
 in
 
 let
   toUnderscoreVersion = version: "ghc-${lib.replaceString "." "_" version}";
+in
+
+let
+  fourmolu-0_12_0_0 =
+    let
+      inherit (pkgs_stackage_22_43.haskell.lib) dontCheck justStaticExecutables;
+    in
+    dontCheck (
+      justStaticExecutables (pkgs_stackage_22_43.haskellPackages.callHackage "fourmolu" "0.12.0.0" { })
+    );
+
+  hlint-3_10 =
+    let
+      inherit (pkgs_stackage_24_38.haskell.lib) justStaticExecutables;
+    in
+    justStaticExecutables (pkgs_stackage_24_38.haskellPackages.callHackage "hlint" "3.10" { });
 in
 
 pkgs.mkShell {
@@ -26,10 +56,12 @@ pkgs.mkShell {
   packages = [
     ghcs-nix.${toUnderscoreVersion ghcVersion}
 
-    (justStaticExecutables (haskellPackages.callCabal2nix "hlint" sources.hlint { }))
+    hlint-3_10
+    fourmolu-0_12_0_0
+
     # apply-refact doesn't build
-    haskellPackages.doctest
-    haskellPackages.fix-whitespace
+    pkgs.haskellPackages.doctest
+    pkgs.haskellPackages.fix-whitespace
 
     pkgs.cabal-install
     pkgs.pkg-config
